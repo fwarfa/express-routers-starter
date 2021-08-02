000467b2f106fd54d8ae9ae01a727146867b2d83
Farah Warfa <fwarfa@gmail.com>
2021-08-02 16:40:16 -0500

    file name updated, added first route to books-router

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const booksRouter = require('./routes/brooks.router');
+const booksRouter = require('./routes/books.router');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
